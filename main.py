1f32bb43a81427810d7127bbee24a5b5267dd854
lmiel <lucia.mielgo@gmail.com>
2023-10-15 20:05:49 +0200

    nombre

---
+++ main.py
@@ -0,0 +1,12 @@
+from pilaHanoi import *
+
+myPila = Pila()
+
+myPila.apilar(5)
+myPila.apilar(4)
+myPila.apilar(2)
+myPila.apilar(5)
+myPila.desapilar()
+
+
+print(myPila)
--- pilaHanoi.py
+++ pilaHanoi.py
@@ -14,8 +14,29 @@ class Pila:
         nodo = nodoPila(valor)
         nodo.debajo = self.cima #nodo. te permite acceder a elementos de la clase
         self.cima = nodo
+        self.tamanio += 1
         
     def desapilar(self):
         ret = self.cima.valor  #aqui self.cima = nodo (nodo.valor) accedemos al valor del nodo cima
-        self.cima = nodo.valor
+        self.cima = self.cima.debajo
+        self.tamanio -= 1
+        return ret
+    
+    def PilaVacia(self):
+        if tamanio == 0:
+            return True
+        else:
+            return False
+        
+    def __str__(self):
+        ret = '['
+        nodo = self.cima
+        for i in range(self.tamanio):
+            ret += str(nodo.valor)
+            ret += ' '
+            nodo = nodo.debajo
+        ret += ']'   
+        return ret
+        
+            
         
